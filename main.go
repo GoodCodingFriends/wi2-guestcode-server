1af05978a32d69975f067b1ca12af7986052318f
Yuki Ito <acomagu@gmail.com>
2018-07-06 01:55:58 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"context"
+	"io/ioutil"
+	"net/http"
+	"net/mail"
+	"regexp"
+	"time"
+
+	"go.mercari.io/datastore"
+	"go.mercari.io/datastore/aedatastore"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
+	gaemail "google.golang.org/appengine/mail"
+)
+
+func sendMail(ctx context.Context) error {
+	msg := &gaemail.Message{
+		Sender:  "Agent <agent@wi2-guestcode-server.appspotmail.com>",
+		To:      []string{"dcs@forguest.wi2.ne.jp"},
+		Subject: "",
+		Body:    "",
+	}
+
+	if err := gaemail.Send(ctx, msg); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type codeEntity struct {
+	Code    string    `datastore:"code"`
+	Used    bool      `datastore:"used"`
+	Created time.Time `datastore:"created"`
+}
+
+func dcsKey(client datastore.Client) datastore.Key {
+	return client.NameKey("code", "dcs", nil)
+}
+
+func main() {
+	handle(http.DefaultServeMux)
+	appengine.Main()
+}
+
+func handle(mux *http.ServeMux) {
+	// Request code
+	mux.HandleFunc("/code", func(w http.ResponseWriter, r *http.Request) {
+		ctx := appengine.NewContext(r)
+		client, err := aedatastore.FromContext(ctx)
+		if err != nil {
+			log.Errorf(ctx, "%v", err)
+			return
+		}
+		defer client.Close()
+
+		var code *codeEntity
+		if _, err := client.RunInTransaction(ctx, func(tx datastore.Transaction) error {
+			var codes []*codeEntity
+			keys, err := client.GetAll(ctx, client.NewQuery("code").Transaction(tx).Ancestor(dcsKey(client)).Filter("used =", false).Order("-created").Limit(1), &codes)
+			if err != nil {
+				return err
+			}
+
+			if len(codes) == 0 || codes[0].Used {
+				log.Infof(ctx, "could not get code")
+
+				w.Write([]byte("wait please"))
+				return nil
+			}
+
+			log.Infof(ctx, "%#+v", codes)
+			code = codes[0]
+			key := keys[0]
+
+			code.Used = true
+
+			if _, err := tx.Put(key, code); err != nil {
+				return err
+			}
+
+			log.Infof(ctx, "updated")
+
+			return nil
+		}); err != nil {
+			log.Errorf(ctx, "%v", err)
+			return
+		}
+
+		if code != nil {
+			w.Write([]byte(code.Code))
+		}
+	})
+
+	// Check code availability cron
+	mux.HandleFunc("/check", func(w http.ResponseWriter, r *http.Request) {
+		ctx := appengine.NewContext(r)
+		client, err := aedatastore.FromContext(ctx)
+		if err != nil {
+			log.Errorf(ctx, "%v", err)
+			return
+		}
+		defer client.Close()
+
+		var codes []*codeEntity
+		if _, err := client.GetAll(ctx, client.NewQuery("code").Ancestor(dcsKey(client)).Filter("used =", false).Order("-created").Limit(1), &codes); err != nil {
+			log.Errorf(ctx, "%v", err)
+		}
+
+		log.Infof(ctx, "%#+v", codes)
+
+		if len(codes) > 0 && !codes[0].Used {
+			log.Infof(ctx, "code available")
+			return
+		}
+
+		if err := sendMail(ctx); err != nil {
+			log.Errorf(ctx, "%v", err)
+			return
+		}
+
+		w.Write([]byte("sent"))
+	})
+
+	// Receive mail
+	mux.HandleFunc("/_ah/mail/", func(w http.ResponseWriter, r *http.Request) {
+		ctx := appengine.NewContext(r)
+		client, err := aedatastore.FromContext(ctx)
+		if err != nil {
+			log.Errorf(ctx, "%v", err)
+			return
+		}
+		defer client.Close()
+
+		msg, err := mail.ReadMessage(r.Body)
+		if err != nil {
+			log.Errorf(ctx, "%v", err)
+			return
+		}
+
+		content, err := ioutil.ReadAll(msg.Body)
+		if err != nil {
+			log.Errorf(ctx, "%v", err)
+			return
+		}
+
+		log.Infof(ctx, "Received mail: %v", string(content))
+
+		code := regexp.MustCompile(`DCS[\w\d]+`).FindString(string(content))
+		if code == "" {
+			log.Infof(ctx, "No code in the reply. Retring to send mail.")
+			if err := sendMail(ctx); err != nil {
+				log.Errorf(ctx, "%v", err)
+				return
+			}
+		} else {
+			k := client.IncompleteKey("code", dcsKey(client))
+			if _, err := client.Put(ctx, k, &codeEntity{
+				Code:    code,
+				Used:    false,
+				Created: time.Now(),
+			}); err != nil {
+				log.Errorf(ctx, "%v", err)
+				return
+			}
+			log.Infof(ctx, "put new record")
+		}
+	})
+}
---
+++ main_test.go
@@ -0,0 +1,182 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"time"
+
+	"go.mercari.io/datastore/aedatastore"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/aetest"
+)
+
+var mailMsgStub = `Date: Mon, 23 Jun 2019 11:40:36 -0400
+From: 株式会社ワイヤ・アンド・ワイヤレス <cs-info@wi2.co.jp>
+To: Agent <agent@wi2-guestcode-server.appspotmail.com>
+Subject: Wi2 300 ゲストコードのお知らせ
+
+Wi2　300　ゲストサービスのお申し込みありがとうございます。
+
+ゲストコードをご確認ください。
+
+■お客様のゲストコード━━━━━━━━━━━━━━━━━━━
+ゲストコード　：　DCS7270BRQ
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+================================================================
+【ご利用いただく前に】
+初回ログインから3時間のご利用が可能です。また、当ゲストコードは
+当施設でご利用ください。
+
+なお、ご利用前には、ログイン画面に掲示してあります「Wi2　フリーWi-Fiサービス
+利用規約」（以下規約）をお読みください。ご利用になられた場合は規約に同意した
+ものとみなします。
+
+ゲストコードによりご利用いただくWi-Fiネットワークの通信は、暗号化
+されておらず通信を傍受される恐れがあります。
+あらかじめご了承頂き、ご利用くださいます様、よろしくお願いいたします。
+================================================================
+
+それでは、インターネットをお楽しみください。
+
+
+■サービスに関するお問い合わせ━━━━━━━━━━━━━━━
+
+　Wi2(ワイツー)カスタマーセンター
+
+　メールフォームによるお問い合わせ
+　⇒https://service.wi2.ne.jp/wi2net/contact/
+　　※本メールアドレスは送信専用です
+
+　お電話によるお問い合わせ
+　⇒0120-858-306（受付時間　10:00～19:00/年中無休）
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+■公衆無線LANサービス　Wi2　300　http://wi2.co.jp/jp/300/
+`
+
+func TestCode(t *testing.T) {
+	mux := http.NewServeMux()
+	handle(mux)
+
+	w := httptest.NewRecorder()
+
+	inst, err := aetest.NewInstance(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer inst.Close()
+
+	req, err := inst.NewRequest("GET", "/code", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ctx := appengine.NewContext(req)
+
+	// Setup datastore
+	client, err := aedatastore.FromContext(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	parentKey, err := client.Put(ctx, client.NameKey("code", "dcs", nil), &struct{}{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	codeKey, err := client.Put(ctx, client.IncompleteKey("code", parentKey), &codeEntity{
+		Code:    "DCS1234ABC",
+		Used:    false,
+		Created: time.Date(1999, 11, 7, 0, 0, 0, 0, time.UTC),
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err := client.Put(ctx, client.IncompleteKey("code", parentKey), &codeEntity{
+		Code:    "DCS5678DEF",
+		Used:    false,
+		Created: time.Date(1996, 7, 30, 0, 0, 0, 0, time.UTC),
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	mux.ServeHTTP(w, req)
+	t.Log(w.Body)
+
+	var updated codeEntity
+	if err := client.Get(ctx, codeKey, &updated); err != nil {
+		t.Fatal(err)
+	}
+
+	if updated.Code != "DCS1234ABC" {
+		t.Errorf("the Code of updated entity is expected DCS1234ABC, but %v", updated.Code)
+	}
+
+	if !updated.Used {
+		t.Errorf("the Used of updated entity is expected true, but %v", updated.Used)
+	}
+}
+
+func TestReceivingMail(t *testing.T) {
+	mux := http.NewServeMux()
+	handle(mux)
+
+	w := httptest.NewRecorder()
+
+	inst, err := aetest.NewInstance(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer inst.Close()
+
+	req, err := inst.NewRequest("POST", "/_ah/mail/agent@wi2-guestcode-server.appspotmail.com", strings.NewReader(mailMsgStub))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ctx := appengine.NewContext(req)
+
+	// Setup datastore
+	client, err := aedatastore.FromContext(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	parentKey, err := client.Put(ctx, client.NameKey("code", "dcs", nil), &struct{}{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err := client.Put(ctx, client.IncompleteKey("code", parentKey), &codeEntity{
+		Code:    "DCS1234ABC",
+		Used:    true,
+		Created: time.Date(1996, 7, 30, 0, 0, 0, 0, time.UTC),
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	mux.ServeHTTP(w, req)
+	t.Log(w.Body)
+
+	var codes []*codeEntity
+	if _, err := client.GetAll(ctx, client.NewQuery("code").Ancestor(dcsKey(client)).Filter("used =", false).Order("-created").Limit(1), &codes); err != nil {
+		t.Fatal(err)
+	}
+
+	if len(codes) != 1 {
+		t.Fatalf("the len of codes got by the query is expected 1, but %v(the value is %v)", len(codes), codes)
+	}
+
+	code := codes[0]
+
+	if code.Code != "DCS7270BRQ" {
+		t.Errorf("the Code of got entity is expected DCS7270BRQ, but %v", code.Code)
+	}
+
+	if code.Used {
+		t.Errorf("the Used of got entity is expected false, but %v", code.Used)
+	}
+}
